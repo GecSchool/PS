f2f5f7bcf5d51c8cd24f24ab7f4a13a81f33706a
Dchool <93516702+GecSchool@users.noreply.github.com>
2022-12-02 16:56:47 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAX_INDEX 9
+int visited[MAX_INDEX];
+int arr[MAX_INDEX];
+int n, m;
+void dfs(int, int);
+int main()
+{
+    scanf("%d %d", &n, &m);
+    dfs(0, 1);
+    return 0;
+}
+void dfs(int len, int index)
+{
+    if (len == m)
+    {
+        for (int i = 0; i < m; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+        return;
+    }
+    for (int i = index; i <= n; i++)
+    {
+        if (!visited[i])
+        {
+            visited[i] = 1;
+            arr[len] = i;
+            dfs(len + 1, i + 1);
+            visited[i] = 0;
+        }
+    }
+}
