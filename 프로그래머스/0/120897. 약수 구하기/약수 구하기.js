5c31d5697fb1bf1cc9d0f5143322931abb4706c4
Dchool <93516702+GecSchool@users.noreply.github.com>
2026-01-13 06:22:48 +0900

    [level 0] Title: 약수 구하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    const result = [];
+    
+    for (let i = 1; i <= Math.sqrt(n); i++){
+        if (n % i === 0) {
+            result.push(i);
+            if (i !== n / i) {
+                result.push(n / i);
+            }
+        }
+    }
+    
+    return result.sort((a, b) => a - b);
+}
