8ffa5ea39a324885fff7d14bd10f6400d232484f
Dchool <93516702+GecSchool@users.noreply.github.com>
2026-01-18 15:17:20 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,17 @@
+function solution(my_string) {
+    const arr = my_string.split(' ');
+    let result = Number(arr[0]);
+    
+    for (let i = 1; i < arr.length; i += 2) {
+        const operator = arr[i];
+        const num = Number(arr[i + 1]);
+        
+        if (operator === '+') {
+            result += num;
+        } else {
+            result -= num; 
+        }
+    }
+    
+    return result;
+}
