9d7f7c7bf3fc587dbca38e6e467d41ae353b30b8
Dchool <93516702+GecSchool@users.noreply.github.com>
2026-01-27 13:35:38 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT
+        ID,
+        NAME,
+        HOST_ID
+FROM (SELECT *, COUNT(*) OVER(PARTITION BY HOST_ID) AS counts FROM PLACES) AS PLACES_COUNT
+WHERE counts >= 2
+ORDER BY ID ASC;
